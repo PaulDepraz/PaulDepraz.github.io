90328973c5788c593fa866c0c73e932879a22546
Paul <deprazz@gmail.com>
2017-05-15 03:07:03 -0300

    ajax to new urls

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-//Inspired by a dribbble by Bluroon
-//https://dribbble.com/shots/1356864-Contact-Us
-
-//Capture the click event on a location
 $("#location-bar a").click(function(event){
   event.preventDefault();
   
@@ -17,3 +13,55 @@ $("#location-bar a").click(function(event){
   //update tooltip 'address'
   $('.selectedLocation').text(selectedLocation);
 });
+
+function send(url, data, verb, success_msg){
+  // Creates Ajax request and alerts the response
+  var xhr = new XMLHttpRequest();
+  xhr.open(verb, url, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  // xhr.setRequestHeader("Access-Control-Allow-Origin", "https://6b0qxqwymf.execute-api.us-east-1.amazonaws.com")
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        alert(success_msg);
+      }
+  };
+  xhr.send(data);
+}
+
+$("#subscribe").submit(function(event){
+  event.preventDefault();
+  event.stopImmediatePropagation();
+  var url = "https://6b0qxqwymf.execute-api.us-east-1.amazonaws.com/beta/subs";
+  var msg = "Your subscription is done, sms incoming!"
+  pn = this[0].value;
+  name = this[1].value;
+  var data = JSON.stringify({
+    'pn': pn,
+    'name': name
+  });
+  send(url, data, "POST", msg);
+  return false;
+});
+
+$("#unsubscribe").submit(function(event){
+  event.preventDefault();
+  event.stopImmediatePropagation();
+  var url = "https://6b0qxqwymf.execute-api.us-east-1.amazonaws.com/beta/subs";
+  var msg = "Done! Feel free to subscribe anytime again!"
+  pn = this[0].value;
+  var data = JSON.stringify({
+    'pn': pn
+  });
+  send(url, data, "DELETE", msg);
+  return false;
+});
+
+$('#pn').keyup(function(event){
+    var pn = $('#pn').val()
+    document.getElementById('phone').innerHTML = "<span class='entypo-phone'></span><a href='#'>"+ pn +"</a>";
+})
+
+$('#name').keyup(function(event){
+    var name = $('#name').val()
+    document.getElementById('user').innerHTML = "<span class='entypo-user'></span><a href='#'>"+ name +"</a>";
+})
